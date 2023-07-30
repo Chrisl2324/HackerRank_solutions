fc0563808d8b38e77cc782df0a43934ce3179bfc
Chris Lombardi <clombar1@email.essex.edu>
2023-07-30 10:46:08 -0400

    Update Set.disarcd(),.remove(),.pop().py

--- Set.disarcd(),.remove(),.pop().py
+++ Set.disarcd(),.remove(),.pop().py
@@ -1,15 +1,22 @@
-n = int(input())
-elements = set(map(int, input().split()))
-N = int(input())
+def set_methods(elements, N):
+    '''Perform different set methods'''
+    for i in range(N):
+        method = input().split()
+        if method[0] == 'pop':
+            elements.pop()
+        elif method[0] == 'remove':
+            elements.remove(int(method[1]))
+        elif method[0] == 'discard':
+            elements.discard(int(method[1]))
+        else:
+            print('Invalid Option!')
+    return sum(elements)
+    
 
-for i in range(N):
-    method = input().split()
-    if method[0] == 'pop':
-        elements.pop()
-    elif method[0] == 'remove':
-        elements.remove(int(method[1]))
-    elif method[0] == 'discard':
-        elements.discard(int(method[1]))
-    else:
-        print('Invalid Option!')
-print(sum(elements))
+if __name__ == '__main__':
+    n = int(input())
+    elements = set(map(int, input().split()))
+    N = int(input())
+    
+    result = set_methods(elements, N)
+    print(result)
